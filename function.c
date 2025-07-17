7941ff01aa3b1d2344606454051dd572def2f4a5
arya-gaikwad26 <gaikwadarya.2569@gmail.com>
2025-07-17 14:15:02 +0000

    17/07

--- function.c
+++ function.c
@@ -1,8 +1,9 @@
-#include<stdio.>
 #include<stdio.h>  
 #include <string.h>    
 int main(){    
-char ch[20]={'T', 'p', 'o', 'i', 'n', 't', 't', 'e', 'c', 'h', '\0'};   
-   printf("Length of string is: %d",strlen(ch));    
+ char ch[20]={'t', 'p', 'o', 'i', 'n', 't', 't', 'e', 'c', 'h', '\0'};     
+   char ch2[20];    
+   strcpy(ch2,ch);    
+   printf("Value of second string is: %s",ch2);    
  return 0;    
 }    
