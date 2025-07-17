da4f7e8a3d994d86c1e91eae6ee2bede184b85b3
arya-gaikwad26 <gaikwadarya.2569@gmail.com>
2025-07-17 14:09:52 +0000

    17/07

---
+++ function.c
@@ -0,0 +1,8 @@
+#include<stdio.>
+#include<stdio.h>  
+#include <string.h>    
+int main(){    
+char ch[20]={'T', 'p', 'o', 'i', 'n', 't', 't', 'e', 'c', 'h', '\0'};   
+   printf("Length of string is: %d",strlen(ch));    
+ return 0;    
+}    
